b9bafc28258cf2afb0d8fd43f41ea6830ac6aa66
lizzie136 <darknessover136@gmail.com>
2018-09-09 11:16:02 -0500

    Integrate Mock up view to service
     - Add JS files to static mock up
     - Update static profile
     - connect to transaction

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/venta', (req, res) => {
+app.get('/transaction', (req, res) => {
   const query = url.parse(req.url, true).query;
   res.send(query);
+  // res.render(path.join(__dirname, 'public/index.html'), query);
 
 });
 
+app.use('/venta/', express.static(path.join(__dirname, 'public')));
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'));
---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+console.log(window.location.toString());
+
+
+const setParameters = (url) => {
+  
+  const params = new URLSearchParams(url.search.slice(1));
+  for(let param of params.entries()){
+    const element = document.querySelector(`[data-model=${param[0]}`);
+    if(!element) continue;
+    element.value  = param[1];
+    if(element.value && param[0] === 'fecha') {
+      element.textContent = moment(param[1]).format('DD/MM/YYYY');
+    }
+  }
+};
+
+window.addEventListener('load', ()=>{
+  setParameters(window.location);
+
+});
