e0557d0349e0a2c4299c3221c03ce09a066d173d
lizzie136 <darknessover136@gmail.com>
2018-09-09 00:02:00 -0500

    Get venta request

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const url = require('url');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/venta', (req, res) => res.send('Hello World!'));
+app.get('/venta', (req, res) => {
+  const query = url.parse(req.url, true).query;
+  res.send(query);
+
+});
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'));
